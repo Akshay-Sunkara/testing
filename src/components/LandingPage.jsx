aa04624aebefe6ec7c2bd6d64a3ea2846a761d1a
Akshay-Sunkara <akshaysunkara68@berkeley.edu>
2025-11-13 16:25:58 -0800

    l

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -105,8 +105,7 @@ function LandingPage() {
                   className="continue-button" 
                   onClick={handleContinue}
                 >
-                  <span>Continue</span>
-                  <svg width="16" height="16" viewBox="0 0 24 24" fill="none">
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="none">
                     <path d="M5 12h14M12 5l7 7-7 7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                   </svg>
                 </button>
